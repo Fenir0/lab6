0c05a6110bf02f66f7d3f8c20bbabcd44e4b5bc9
Fenir0 <feniro.chur@gmail.com>
2025-04-24 08:52:07 +0700

    Update3

--- src/sq_matr.h
+++ src/sq_matr.h
@@ -3,13 +3,15 @@
 
 #include "matr.h"
 
-struct square_matrix: public matrix{
+class square_matrix: public matrix{
+public:
     square_matrix();
     square_matrix(int n);
 
     int determinant();
     bool is_symmetrical();
     square_matrix power(int n);
+protected:
     square_matrix& operator* (square_matrix& res);
 };
 
